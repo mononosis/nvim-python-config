43a9c31d1194d102d6d10a2bf52cdfec9f36e2c4
Your Name <you@example.com>
2023-12-03 09:39:43 +0000

    test

--- init.lua
+++ init.lua
@@ -1,14 +1,41 @@
-require'lspconfig'.pyright.setup{
-    on_attach = function(client, bufnr)
-        -- Key mappings and other configuration that should only be set
-        -- after the language server attaches to the current buffer
-    end,
-    settings = {
-        python = {
-            analysis = {
-                typeCheckingMode = "strict"  -- Adjust as needed ("off", "basic", "strict")
-            }
-        }
+vim.cmd('color happy_hacking')
+
+require 'lspconfig'.pyright.setup {
+  settings = {
+    python = {
+      analysis = {
+        typeCheckingMode = "basic" -- Adjust as needed ("off", "basic", "strict")
+      }
     }
+  }
 }
 
+require('formatter').setup({
+  filetype = {
+    python = {
+      -- Black
+      function()
+        return {
+          exe = "black",
+          args = { "-t", "py311", "-" },
+          stdin = true
+        }
+      end
+    }
+  }
+})
+
+vim.api.nvim_set_keymap('n', '<leader>f', '<cmd>Format<CR>', { noremap = true, silent = true })
+
+local null_ls = require("null-ls")
+
+null_ls.setup({
+  sources = {
+    null_ls.builtins.diagnostics.flake8.with({
+      -- Specify `flake8` options here
+      extra_args = { "--max-line-length=100", "--ignore=E203,W503" },
+    }),
+  },
+})
+
+vim.api.nvim_set_keymap('n', '<leader>t', '<cmd>!python %<CR>', { noremap = true, silent = true })
