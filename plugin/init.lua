8367bd4df2710f2d0620ed2f74e4eb8f7a6cbb79
Your Name <you@example.com>
2026-04-15 16:15:01 +1000

    first

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,4 +1,4 @@
-vim.cmd('color happy_hacking')
+vim.cmd('color cyberpunk')
 
 local lspconfig = require('lspconfig')
 
@@ -10,13 +10,27 @@ local function on_attach(_, buffer)
 end
 lspconfig.pyright.setup {
   on_attach = on_attach,
+  cmd = { "pyright-langserver", "--stdio" },
+  filetypes = { "python" },
+  --root_dir = function(filename)
+    --return util.root_pattern(unpack(root_files))(filename) or util.path.dirname(filename),
+  --end,
   settings = {
     python = {
       analysis = {
-        typeCheckingMode = "basic" -- Adjust as needed ("off", "basic", "strict")
+        autoSearchPaths = true,
+        diagnosticMode = "workspace",
+        useLibraryCodeForTypes = true
       }
     }
   }
+  --settings = {
+  --python = {
+  --analysis = {
+  --typeCheckingMode = "basic" -- Adjust as needed ("off", "basic", "strict")
+  --}
+  --}
+  --}
 }
 
 --require 'lspsaga'.setup {
@@ -57,7 +71,33 @@ vim.api.nvim_set_keymap('n', '<leader>f', '<cmd>Format<CR>', { noremap = true, s
 --},
 --})
 
-vim.api.nvim_set_keymap('n', '<leader>t', '<cmd>!python %<CR>', { noremap = true, silent = true })
+
+vim.keymap.set('n', '<leader>t', function()
+  vim.cmd('w')
+  local file = vim.api.nvim_buf_get_name(0)
+
+  local buf = vim.api.nvim_create_buf(false, true)
+  local w = math.floor(vim.o.columns * 0.8)
+  local h = math.floor(vim.o.lines * 0.6)
+  local win = vim.api.nvim_open_win(buf, true, {
+    relative = 'editor',
+    row = math.floor((vim.o.lines - h) / 2 - 1),
+    col = math.floor((vim.o.columns - w) / 2),
+    width = w, height = h,
+    style = 'minimal',
+    border = 'rounded',
+    title = ' terminal: python ' .. vim.fn.fnamemodify(file, ':t') .. ' ',
+    title_pos = 'center',
+  })
+
+  vim.fn.termopen({ 'python', file })
+  vim.cmd('startinsert')
+
+  -- Esc to normal; q to close
+  vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { buffer = buf, silent = true })
+  vim.keymap.set('n', 'q', function() pcall(vim.api.nvim_win_close, win, true) end,
+    { buffer = buf, silent = true })
+end, { desc = 'Run current file (float terminal)' })
 
 local dap = require('dap')
 dap.configurations.python = {
@@ -78,22 +118,15 @@ dap.adapters.python = {
 }
 
 require("nvim-dap-virtual-text").setup {
-    enabled = true,                        -- enable this plugin (the default)
-    enabled_commands = true,               -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
-    highlight_changed_variables = true,    -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
-    highlight_new_as_changed = false,      -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
-    show_stop_reason = true,               -- show stop reason when stopped for exceptions
-    commented = false,                     -- prefix virtual text with comment string
-    only_first_definition = true,          -- only show virtual text at first definition (if there are multiple)
-    all_references = false,                -- show virtual text on all all references of the variable (not only definitions)
-    clear_on_continue = false,             -- clear virtual text on "continue" (might cause flickering when stepping)
-    --- A callback that determines how a variable is displayed or whether it should be omitted
-    --- @param variable Variable https://microsoft.github.io/debug-adapter-protocol/specification#Types_Variable
-    --- @param buf number
-    --- @param stackframe dap.StackFrame https://microsoft.github.io/debug-adapter-protocol/specification#Types_StackFrame
-    --- @param node userdata tree-sitter node identified as variable definition of reference (see `:h tsnode`)
-    --- @param options nvim_dap_virtual_text_options Current options for nvim-dap-virtual-text
-    --- @return string|nil A text how the virtual text should be displayed or nil, if this variable shouldn't be displayed
+    enabled = true,
+    enabled_commands = true,
+    highlight_changed_variables = true,
+    highlight_new_as_changed = false,
+    show_stop_reason = true,
+    commented = false,
+    only_first_definition = true,
+    all_references = false,
+    clear_on_continue = false,
     display_callback = function(variable, buf, stackframe, node, options)
       if options.virt_text_pos == 'inline' then
         return ' = ' .. variable.value
@@ -101,41 +134,10 @@ require("nvim-dap-virtual-text").setup {
         return variable.name .. ' = ' .. variable.value
       end
     end,
-    -- position of virtual text, see `:h nvim_buf_set_extmark()`, default tries to inline the virtual text. Use 'eol' to set to end of line
     virt_text_pos = vim.fn.has 'nvim-0.10' == 1 and 'inline' or 'eol',
-
-    -- experimental features:
-    all_frames = false,                    -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
-    virt_lines = false,                    -- show virtual lines instead of virtual text (will flicker!)
-    virt_text_win_col = nil                -- position the virtual text at a fixed window column (starting from the first text column) ,
-                                           -- e.g. 80 to position at column 80, see `:h nvim_buf_set_extmark()`
+    all_frames = false,
+    virt_lines = false,
+    virt_text_win_col = nil,
 }
 require("dapui").setup()
 vim.cmd[[hi Normal guibg=NONE ctermbg=NONE]]
-
-
---dap.adapters.python = function(cb, config)
---if config.request == 'attach' then
------@diagnostic disable-next-line: undefined-field
---local port = (config.connect or config).port
------@diagnostic disable-next-line: undefined-field
---local host = (config.connect or config).host or '127.0.0.1'
---cb({
---type = 'server',
---port = assert(port, '`connect.port` is required for a python `attach` configuration'),
---host = host,
---options = {
---source_filetype = 'python',
---},
---})
---else
---cb({
---type = 'executable',
---command = 'path/to/virtualenvs/debugpy/bin/python',
---args = { '-m', 'debugpy.adapter' },
---options = {
---source_filetype = 'python',
---},
---})
---end
---end
